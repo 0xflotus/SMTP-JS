47b8108871caaabf773d505230df17361e02b00b
MaXoooZ <55662431+Max-xoo@users.noreply.github.com>
2020-10-14 19:33:44 +0200

    Switching to async

--- index.js
+++ index.js
@@ -2,24 +2,27 @@ const FS = require("fs");
 const CG = require("./SMTP-CONFIG/infos.json");
 const MAIL = require("nodemailer");
 
-FS.readFile("SMTP-CONFIG/mails_list.txt", "utf-8", function (err, data) {
-    try {
-        if (err) throw err;
-        const array = data.split("\n");
+SEND();
 
-        for (const mails of array) {
-            const transporter = MAIL.createTransport({
-                service: "gmail",
-                auth: { user: CG.yourmail, pass: CG.yourpassword }
-            });
+async function SEND() {
+    FS.readFile("SMTP-CONFIG/mails_list.txt", "utf-8", function (err, data) {
+        try {
+            const array = await data.split("\n");
 
-            const mailOptions = { from: CG.yourmail, to: mails, subject: CG.subject, text: CG.texts };
+            for (const mails of array) {
+                const transporter = await MAIL.createTransport({
+                    service: "gmail",
+                    auth: { user: CG.yourmail, pass: CG.yourpassword }
+                });
 
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (!error) console.log("Email sent to: " + info.response);
-            });
+                const mailOptions = { from: CG.yourmail, to: mails, subject: CG.subject, text: CG.texts };
+
+                transporter.sendMail(mailOptions, function (error, info) {
+                    if (!error) console.log("Email sent to: " + info.response);
+                });
+            }
+        } catch (err) {
+            console.error(err.message)
         }
-    } catch (err) {
-        console.error(err.message)
-    }
-});
+    });
+}
